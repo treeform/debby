02b0126518e33b0512ee1951191a4cf9f553db6a
treeform <starplant@gmail.com>
2024-02-08 10:29:52 -0800

    Add tests for DbErrors for postgres.

--- tests/test_postgres.nim
+++ tests/test_postgres.nim
@@ -9,4 +9,40 @@ let db = openDatabase(
 
 include common_test
 
+block:
+  # Test PG unique constraint
+
+  type
+    UniqueName = ref object
+      id: int
+      name: string
+
+  db.dropTableIfExists(UniqueName)
+  db.createTable(UniqueName)
+
+  db.query("CREATE UNIQUE INDEX unique_name_idx ON unique_name (name)")
+
+  db.insert(UniqueName(name: "hello"))
+  doAssertRaises(DbError):
+    db.insert(UniqueName(name: "hello"))
+
+block:
+  # Test PG wrong type
+
+  type
+    WrongType = ref object
+      id: int
+      num: int
+      name: string
+
+  db.dropTableIfExists(WrongType)
+  db.createTable(WrongType)
+
+  doAssertRaises(DbError):
+    db.query(
+      "INSERT INTO wrong_type (num, name) VALUES (?,?)",
+      "hello",
+      123
+    )
+
 db.close()
